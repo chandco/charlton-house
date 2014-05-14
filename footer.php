eb2ab58aabc260c7adcefbdd92ef81c7ff935a70
Nathan Edwards <nathan.edwards@chandco.net>
2014-05-14 11:45:59 +0100

    URL for minified JS fixed

--- footer.php
+++ footer.php
@@ -198,7 +198,7 @@
 <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>-->
 <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/min/main.min.js"></script>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
